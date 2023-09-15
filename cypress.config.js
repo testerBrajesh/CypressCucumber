837a06980b67783571d588331ccfb802927efa06
testerBrajesh <bkewat582@gmail.com>
2023-09-15 18:03:35 +0530

    integrate report

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,11 @@ const cucumber = require('cypress-cucumber-preprocessor').default
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter', ///for html report
   e2e: {
     setupNodeEvents(on, config) {
+      this.screenshotOnRunFailure= true;
+      require('cypress-mochawesome-reporter/plugin')(on);
       on('file:preprocessor', cucumber()) //For cypress cucumber preprocessor
     },
     specPattern: "cypress/e2e/*.feature"
